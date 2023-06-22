3b5333daecc77f36f340a6315ae064596deddbc2
RiAliotta <127410611+RiAliotta@users.noreply.github.com>
2023-06-22 17:51:12 +0200

    Graphical tidying of the code in "kinematics.m"

--- kinematics.m
+++ kinematics.m
@@ -21,24 +21,21 @@ r1 = C1-C;
 r2 = C2-C;
 r3 = C3-C;
 r4 = C4-C;
-I_link1 = [0 0 0;
-        0 1/12*m_link*l^2 0;
-        0 0 1/12*m_link*l^2]
-I_link2 = [cos(q1) -sin(q1) 0;
-    sin(q1) cos(q1) 0;
-    0 0 1]*I_link1*[cos(q1) -sin(q1) 0;
-    sin(q1) cos(q1) 0;
-    0 0 1]'
-I_link3 = [cos(q1+pi/2) -sin(q1+pi/2) 0;
-    sin(q1+pi/2) cos(q1+pi/2) 0;
-    0 0 1]*I_link1*[cos(q1+pi/2) -sin(q1+pi/2) 0;
-    sin(q1+pi/2) cos(q1+pi/2) 0;
-    0 0 1]'
-I_link4 = [cos(q1+pi/2+q2) -sin(q1+pi/2+q2) 0;
-    sin(q1+pi/2+q2) cos(q1+pi/2+q2) 0;
-    0 0 1]*I_link1*[cos(q1+pi/2+q2) -sin(q1+pi/2+q2) 0;
-    sin(q1+pi/2+q2) cos(q1+pi/2+q2) 0;
-    0 0 1]'
+R_12 = [cos(q1) -sin(q1) 0;
+        sin(q1) cos(q1)  0;
+        0       0        1];
+R_23 = [cos(q1+pi/2) -sin(q1+pi/2) 0;
+        sin(q1+pi/2) cos(q1+pi/2)  0;
+        0            0             1];
+R_34 = [cos(q1+pi/2+q2) -sin(q1+pi/2+q2) 0;
+        sin(q1+pi/2+q2) cos(q1+pi/2+q2)  0;
+        0               0                1];
+I_link1 = [0 0               0;
+           0 1/12*m_link*l^2 0;
+           0 0               1/12*m_link*l^2];
+I_link2 = R_12*I_link1*R_12';
+I_link3 = R_23*I_link1*R_23';
+I_link4 = R_34*I_link1*R_34';
 I = I_link1+I_link2+I_link3+I_link4+m_link*(r1'*r1*eye(3)-r1*r1'+r2'*r2*eye(3)-r2*r2'+r3'*r3*eye(3)-r3*r3'+r4'*r4*eye(3)-r4*r4')
 
 %% Allocation matrix computation
@@ -48,26 +45,20 @@ ki = 0.01;
 % syms beta ki real
 
 R_F1C = [cos(-beta) 0 sin(-beta);
-    0 1 0;
-    -sin(-beta) 0 cos(-beta)];
+         0          1 0;
+        -sin(-beta) 0 cos(-beta)];
 
 R_F2C = [cos(q1) -sin(q1) 0;
-    sin(q1) cos(q1) 0;
-    0 0 1]*[cos(beta) 0 sin(beta);
-    0 1 0;
-    -sin(beta) 0 cos(beta)];
+         sin(q1) cos(q1) 0;
+         0 0 1]*R_F1C;
 
 R_F3C = [cos(q1+pi/2) -sin(q1+pi/2) 0;
     sin(q1+pi/2) cos(q1+pi/2) 0;
-    0 0 1]*[cos(-beta) 0 sin(-beta);
-    0 1 0;
-    -sin(-beta) 0 cos(-beta)];
+    0 0 1]*R_F1C;
 
 R_F4C = [cos(q1+pi/2+q2) -sin(q1+pi/2+q2) 0;
     sin(q1+pi/2+q2) cos(q1+pi/2+q2) 0;
-    0 0 1]*[cos(beta) 0 sin(beta);
-    0 1 0;
-    -sin(beta) 0 cos(beta)];
+    0 0 1]*R_F1C;
 
 Qp_tran = [R_F1C*[0; 0; 1], R_F2C*[0; 0; 1], R_F3C*[0; 0; 1], R_F4C*[0; 0; 1]];
 
@@ -78,11 +69,12 @@ us = m*9.81/norm(Qp_tran*up)*up;
 fs = Qp_tran*us;
 alfa_x = atan2(fs(2), fs(3));
 alfa_y = atan2(-fs(1), sqrt(fs(2)^2+fs(3)^2));
-R_C_CoG = [cos(alfa_y) 0 sin(alfa_y);
-    0 1 0;
-    -sin(alfa_y) 0 cos(alfa_y)]*[1 0 0;
-    0 cos(alfa_x) -sin(alfa_x);
-    0 sin(alfa_x) cos(alfa_x)];
+R_C_CoG = [cos(alfa_y)  0 sin(alfa_y);
+           0            1 0;
+           -sin(alfa_y) 0 cos(alfa_y)]*...
+           [1 0           0;
+            0 cos(alfa_x) -sin(alfa_x);
+            0 sin(alfa_x) cos(alfa_x)];
 Q_tran = R_C_CoG*Qp_tran;
 Q_rot = R_C_CoG*Qp_rot;
 
